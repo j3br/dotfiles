027d47d1db21308d2b64b37bd82a9c8d4c1bcc61
j3br <11167461+j3br@users.noreply.github.com>
2024-07-10 21:39:22 +0200

    chore: update dotfiles

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,7 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { theme = 'poimandres' }
+M.plugins = "custom.plugins"
+
+return M
--- .config/nvim/lua/plugins/myplugins.lua
+++ .config/nvim/lua/plugins/myplugins.lua
@@ -17,12 +17,12 @@ local plugins = {
       },
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    config = function ()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  }
+  -- {
+    -- "neovim/nvim-lspconfig",
+    -- config = function ()
+      -- require "plugins.configs.lspconfig"
+      -- require "custom.configs.lspconfig"
+    -- end,
+  -- }
 }
 return plugins
