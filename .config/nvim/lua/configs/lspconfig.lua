26945d911583420352c7464d18260b548ebb917f
j3br <11167461+j3br@users.noreply.github.com>
2025-01-28 19:24:38 +0100

    chore: update nvim config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "pyright", "astro", "tsserver" }
+local servers = { "html", "cssls", "clangd", "pyright", "astro", "ts_ls", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
